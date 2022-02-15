cbe9ef9186f54d71e69f4da722237316e76d817c
José luis <awsjoncs@gmail.com>
2022-02-16 00:25:57 +0100

    commit el leccion 43

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+import { HeroeComponent } from './heroes/heroe/heroe.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent,
+    HeroeComponent
   ],
+
   imports: [
     BrowserModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
